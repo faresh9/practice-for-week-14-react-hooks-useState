614bc6d025c62aa522b0efd9ed8e057928dbac62
faresh9 <93577791+faresh9@users.noreply.github.com>
2023-08-06 19:24:17 +0300

    setting slice

--- src/components/UseState/index.js
+++ src/components/UseState/index.js
@@ -1,11 +1,26 @@
 import "./UseState.css";
-
+import { useState } from "react";
 const UseState = () => {
+  const [theme, setTheme] = useState("light");
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="theme">
+    <div className={`state = ${theme}`}>
       <h1>UseState Component</h1>
-      <button>Dark</button>
-      <button>Light</button>
+      <button
+        onClick={() => {
+          setTheme("dark");
+        }}
+      >
+        Dark
+      </button>
+      <button
+        onClick={() => {
+          setTheme("light");
+        }}
+      >
+        Light
+      </button>
       <h2>DISPLAY COUNT HERE</h2>
       <button>Increment</button>
       <button>Decrement</button>
